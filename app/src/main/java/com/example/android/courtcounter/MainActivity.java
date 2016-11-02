9bd8d885271518d8b7f57f4132f7cc4f2f563cd3
Eric Wickstrom <wickstrome@gmail.com>
2016-11-02 13:20:01 -0500

    add team A views, methods for updating score

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // scores
+    private int mTeamAScore;
+    private int mTeamBScore;
+
+    // views
+    private Button mTeamAAddThreePointsButton;
+    private Button mTeamAAddTwoPointsButton;
+    private Button mTeamAAddFreeThrowButton;
+
+    private TextView mTeamAScoreTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTeamAScore = 0;
+        mTeamBScore = 0;
+
+        mTeamAScoreTV = (TextView) findViewById(R.id.teamAScoreTV);
+
+        mTeamAAddThreePointsButton = (Button) findViewById(R.id.teamAThreePointsButton);
+        mTeamAAddThreePointsButton.setOnClickListener(this);
+
+        mTeamAAddTwoPointsButton = (Button) findViewById(R.id.teamATwoPointsButton);
+        mTeamAAddTwoPointsButton.setOnClickListener(this);
+
+        mTeamAAddFreeThrowButton = (Button) findViewById(R.id.teamAFreeThrowButton);
+        mTeamAAddFreeThrowButton.setOnClickListener(this);
+
+    }
+
+    private void updateTeamAScore(){
+        mTeamAScoreTV.setText(String.valueOf(mTeamAScore));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.teamAThreePointsButton:
+                mTeamAScore += 3;
+                updateTeamAScore();
+                break;
+
+            case R.id.teamATwoPointsButton:
+                mTeamAScore += 2;
+                updateTeamAScore();
+                break;
+
+            case R.id.teamAFreeThrowButton:
+                mTeamAScore++;
+                updateTeamAScore();
+                break;
+        }
     }
 }
